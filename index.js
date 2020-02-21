3201749e1195189c1d9a701f7b3b21aa79919b74
Ryan Kaplan <ryankaplan7@gmail.com>
2020-02-21 14:46:40 -0500

    updated index

--- index.js
+++ index.js
@@ -16,32 +16,6 @@ function writeToFile(fileName, data) {
 
 function init() {
 
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "Enter your GitHub username:",
-                name: "username"
-            },
-            {
-                type: "input",
-                message: "What is the name of your repo?",
-                name: "reponame"
-            }
-        ])
-
-        .then(function (response) {
-
-            const queryUrl = `https://api.github.com/users/${response.username}/repos?per_page=100`;
-
-            axios.get(queryUrl).then(function (res) {
-
-                const repoDetails = res.data.filter(function (res) {
-                    return res.name === response.reponame;
-
-                });
-                console.log(repoDetails);
-            });
-        });
+    console.log(api);
 }
 init();
