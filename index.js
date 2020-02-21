f481c6593d6d5f15f5a7bd53926965dbbf4416a3
Ryan Kaplan <ryankaplan7@gmail.com>
2020-02-21 14:43:41 -0500

    updated

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-const questions = [
+// const apiDoc = require('./api');
+// const generateMarkdownDoc = require('./generateMarkdown');
+const axios = require('axios').default;
+const inquirer = require("inquirer");
+const fs = require('fs');
+
+const questions = ["What is your GitHUb username?",
 
 ];
+
 
 function writeToFile(fileName, data) {
 }
 
+
+
 function init() {
 
-}
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter your GitHub username:",
+                name: "username"
+            },
+            {
+                type: "input",
+                message: "What is the name of your repo?",
+                name: "reponame"
+            }
+        ])
 
+        .then(function (response) {
+
+            const queryUrl = `https://api.github.com/users/${response.username}/repos?per_page=100`;
+
+            axios.get(queryUrl).then(function (res) {
+
+                const repoDetails = res.data.filter(function (res) {
+                    return res.name === response.reponame;
+
+                });
+                console.log(repoDetails);
+            });
+        });
+}
 init();
--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,33 @@
 const api = {
   getUser(username) {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "Enter your GitHub username:",
+          name: "username"
+        },
+        {
+          type: "input",
+          message: "What is the name of your repo?",
+          name: "reponame"
+        }
+      ])
+
+      .then(function (response) {
+
+        const queryUrl = `https://api.github.com/users/${response.username}/${response.reponame}/?per_page=100`;
+
+        axios.get(queryUrl).then(function (res) {
+
+          // const repoDetails = res.data.filter(function (res) {
+          //   return res.name === response.reponame;
+          console.log(res);
+          // });
+          // console.log(repoDetails);
+        });
+      });
+
 
   }
 };
